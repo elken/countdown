3daf1f4dccf4ddc17f18780b34c481b1f59a8ef0
Ellis Kenyo <me@elken.dev>
2023-01-19 16:04:43 +0000

    init

---
+++ src/bot.clj
@@ -0,0 +1,61 @@
+(ns bot
+  (:require
+   [clojure.core.async    :as a]
+   [discljord.connections :as c]
+   [discljord.messaging   :as m]
+   [discljord.permissions :as p]
+   [cprop.core            :refer [load-config]]
+   [clojure.string        :as s])
+  (:gen-class))
+
+(defn -main [& args]
+  (let [event-ch      (a/chan 100)
+        {:keys [token guild-id message-prefix event-date]} (load-config)
+        connection    (c/connect-bot! token event-ch :intents #{:guilds :guild-messages})
+        conn          (m/start-connection! token)
+        event-start   (java.time.Instant/parse event-date)]
+
+    (defn channel-name []
+      (let [diff          (- (.toEpochMilli event-start) (.toEpochMilli (java.time.Instant/now)))
+            days          (quot diff 86400000)
+            hours         (mod (quot diff 3600000) 24)
+            mins          (mod (-> (quot diff 1000)
+                                   (quot 60))
+                               60)]
+        (if (.isAfter event-start (java.time.Instant/now))
+          (str message-prefix " " days "D " hours "H " mins "M")
+          (str message-prefix " happening!"))))
+
+    (defn channel-starts-with? [prefix]
+      (let [channels (a/<!! (m/get-guild-channels! conn guild-id))]
+        (first (filter #(s/starts-with? (:name %) prefix) channels))))
+
+    (defn public-role []
+      (let [roles (a/<!! (m/get-guild-roles! conn guild-id))]
+        (first (filter #(= (:name %) "@everyone") roles))))
+
+    (try
+      (loop []
+        (let [[event-type] (a/<!! event-ch)
+              channel (channel-starts-with? message-prefix)]
+
+          (if channel
+            (a/<!! (m/modify-channel! conn (:id channel) {:name (channel-name)}))
+            (a/<!! (m/create-guild-channel!
+                    conn
+                    guild-id
+                    (channel-name)
+                    {:type 2
+                     :permission-overwrites [{:id (:id (public-role))
+                                              :type 0
+                                              :allow 0
+                                              :deny (:connect p/permissions-bit)}]})))
+
+          ;; TODO This could probably do with a Thread/sleep
+          (when-not (= :disconnect event-type)
+            (recur))))
+      (finally
+        (m/stop-connection! conn)
+        (c/disconnect-bot!  connection)
+        (a/close!           event-ch))))
+  )
